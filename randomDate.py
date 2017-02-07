f3f85583f5cd5419110811feb26687e00e3d2ad8
demon song <787423495@qq.com>
2017-02-07 14:30:04 +0800

    随机日期生成实现

--- randomDate.py
+++ randomDate.py
@@ -1,20 +1,7 @@
-import numpy as np
-from scipy import stats
-
-# 模拟1000个服从泊松分布的随机变量？
-data = stats.poisson.rvs(mu = 10,loc = 0,size = 12)
-
-# 现在要生成2017年 某个月的交易数据  每个月分为四周，平均每周交易10笔
-
-# 生成一个季度的交易数据 总共有 3 *４　＝　１２周
-
-# 把对应的每周交易数随机落在某个时间段
-
-# 随机生成指定时间段内的日期
 import datetime
 import random
     
-
+# 随机生成小时范围
 startHours = "00:00:00"
 endHours = "23:59:59"
 
@@ -28,9 +15,6 @@ def seconds2time(sec):
     h,m = divmod(m,60)
     return "%02d:%02d:%02d" % (h,m,s)
 
-startDate = "2017-01-01"
-endDare = "2017-01-07"
-
 def time2date(t):
     m,d = t.strip().split("-")
     return int(m) * 30 + int(d)
@@ -39,6 +23,7 @@ def date2time(year,date):
     m,d = divmod(date,30)
     return "%d-%02d-%02d" % (year,m,d)
 
+# year 暂时不起作用 startDate 的格式为 "01-07",num 为生成的随机日期的个数
 def randomDate(year,startDate,endDate,num):
     sd = time2date(startDate)
     ed = time2date(endDate)
@@ -56,6 +41,3 @@ def randomDate(year,startDate,endDate,num):
     
     return dates
     
-
-
-
---
+++ trade.py
@@ -0,0 +1,6 @@
+class Trade:
+    def __init__(self,date):
+        self.date = date
+
+    def printDate(self):
+        print("transaction date is " + self.date)
